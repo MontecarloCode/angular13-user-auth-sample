a718ee3f95b52546c4e37ea3e693ad1aa673914b
montecarlocode <monte00829@gmail.com>
2022-06-27 14:04:26 +0800

    ''

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: 'AIzaSyC8E3g2vM-b01TZqEavUR31UZuIRaZtbCo',
-        authDomain: 'neymarjohn215-c7233.firebaseapp.com',
-        databaseURL: 'https://neymarjohn215-c7233-default-rtdb.firebaseio.com',
-        projectId: 'neymarjohn215-c7233',
-        storageBucket: 'neymarjohn215-c7233.appspot.com',
-        messagingSenderId: '147198233371',
-        appId: '1:147198233371:web:559fab2eb47bf3b73161be',
+        apiKey: '',
+        authDomain: '',
+        databaseURL: '',
+        projectId: '',
+        storageBucket: '',
+        messagingSenderId: '',
+        appId: '',
     },
 };
 
